6811899d6ff02d04df3e96d1359de9b466afec9e
mreichhoff <matthew.reichhoff@gmail.com>
2017-02-18 12:49:12 -0600

    initial commit to handle timed text subtitle format

--- audioSubtitles.py
+++ audioSubtitles.py
@@ -3,11 +3,18 @@ import re
 from pydub import AudioSegment
 import argparse
 import os
+import xml.etree.ElementTree as ET
 
-def extractAnswers(lines):
+def extractAnswers(filename):
+    fileExtension = os.path.splitext(filename)[1]
     output = []
-    for line in lines:
-        output.append(extractAnswer(line))
+    if(fileExtension == ".txt"):
+        lines = readInput(filename)
+        for line in lines:
+            output.append(extractAnswer(line))
+    elif(fileExtension == ".xml"):
+        timedText = ET.ElementTree().parse(filename)
+        output = [x.text for x in timedText[0].findall("p")]
     return output
 
 def extractAnswer(line):
@@ -16,11 +23,21 @@ def extractAnswer(line):
         return inputs[1]
     return "error"
 
-def extractTimings(lines):
+#TODO handle this txt vs xml stuff more cleanly
+def extractTimings(filename):
+    fileExtension = os.path.splitext(filename)[1]
     output = []
-    #TODO list comprehension here
-    for line in lines:
-        output.append(extractTiming(line))
+    if(fileExtension == ".txt"):
+        #TODO list comprehension here
+        #TODO readInput is done twice and involves file IO...fix
+        lines = readInput(filename)
+        for line in lines:
+            output.append(extractTiming(line))
+    elif(fileExtension == ".xml"):
+        #handle timed text format (as seen on youtube)
+        timedText = ET.ElementTree().parse(filename)
+        output = [(int(x.attrib['t']),int(x.attrib['t'])+int(x.attrib['d'])) for x in timedText[0].findall("p")]
+
     return output
 
 def extractTiming(line):
@@ -70,19 +87,17 @@ parser.add_argument("audioFilename", help="")
 parser.add_argument("subtitlesFilename", help="")
 args = parser.parse_args()
 
+#TODO: audio file type independence
 audio = AudioSegment.from_file(args.audioFilename, "mp3")
-lines = readInput(args.subtitlesFilename)
-
-timings = extractTimings(lines)
-answers = extractAnswers(lines)
+timings = extractTimings(args.subtitlesFilename)
+answers = extractAnswers(args.subtitlesFilename)
 
 for timing in timings:
     clip = audio[timing[0]:timing[1]]
 
-    #TODO pass this in as an arg instead of hardcoding
+    #TODO pass this in as an arg instead of hardcoding...make filename stuff less ugly
     out_f = open("/Users/mreichhoff/Library/Application Support/Anki2/User 1/collection.media/" + os.path.split(args.audioFilename)[1].replace(" ", "").replace(".mp3","")+str(timing[0]) + str(timing[1])  + ".mp3", 'wb')
     clip.export(out_f, format='mp3')
 
 for index,answer in enumerate(answers):
     print("[sound:" + os.path.split(args.audioFilename)[1].replace(" ", "").replace(".mp3","")+str(timings[index][0]) + str(timings[index][1])  + ".mp3]" + ";" + answers[index])
-        
